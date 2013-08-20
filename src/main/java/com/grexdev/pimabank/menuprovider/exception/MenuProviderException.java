d8cbef3825df930be81d00b1a2a295f5dd8d29ee
grexag <grexag@gmail.com>
2013-08-20 12:46:39 -0700

    Maven artifacts for 0.0.22

--- src/main/java/com/grexdev/pimabank/menuprovider/exception/MenuProviderException.java
+++
@@ -1,9 +0,0 @@
-package com.grexdev.pimabank.menuprovider.exception;
-
-public class MenuProviderException extends Exception {
-    
-    public MenuProviderException(String message, Throwable cause) {
-        super(message, cause);
-    }
-
-}
